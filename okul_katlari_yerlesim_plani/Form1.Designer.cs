4eadf1e5ab497ec0ca99966f4d2b65c15ce7b6a6
İsmail İliksiz <ismail.iliksiz@mrevyap.k12.tr>
2021-10-08 22:24:15 +0300

    Add project files.

---
+++ okul_katlari_yerlesim_plani/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace okul_katlari_yerlesim_plani
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.kat = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.göster = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.göster);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.kat);
+            this.groupBox1.Location = new System.Drawing.Point(23, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(529, 262);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Okul kat yerleşim planı";
+            // 
+            // kat
+            // 
+            this.kat.Location = new System.Drawing.Point(123, 60);
+            this.kat.Name = "kat";
+            this.kat.Size = new System.Drawing.Size(100, 22);
+            this.kat.TabIndex = 0;
+            this.kat.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kat numarası:";
+            // 
+            // göster
+            // 
+            this.göster.Location = new System.Drawing.Point(247, 57);
+            this.göster.Name = "göster";
+            this.göster.Size = new System.Drawing.Size(75, 23);
+            this.göster.TabIndex = 2;
+            this.göster.Text = "Göster";
+            this.göster.UseVisualStyleBackColor = true;
+            this.göster.Click += new System.EventHandler(this.göster_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(20, 102);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(317, 135);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 300);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox kat;
+        private System.Windows.Forms.Button göster;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ okul_katlari_yerlesim_plani/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace okul_katlari_yerlesim_plani
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        int katnumarasi;    
+        private void göster_Click(object sender, EventArgs e)
+        {
+            katnumarasi = int.Parse(kat.Text);
+            if (katnumarasi == 1)
+            {
+                richTextBox1.Text = " 9/A Sınıfı \n 9/B Sınıfı \n 9/C Sınıfı \n 9/D Sınıfı";
+            }
+            else if (katnumarasi == 2)
+            {
+                richTextBox1.Text = " 10/A Sınıfı \n 10/B Sınıfı \n 10/C Sınıfı \n 10/D Sınıfı";
+            }
+            else if (katnumarasi == 3)
+            {
+                richTextBox1.Text = " 11/A Sınıfı \n 11/B Sınıfı \n 11/C Sınıfı \n 11/D Sınıfı";
+            }
+            else
+            {
+                MessageBox.Show("Geçersiz rakam girdiniz, geçerli rakamlar:1,2,3");
+            }
+
+            
+        }
+    }
+}
